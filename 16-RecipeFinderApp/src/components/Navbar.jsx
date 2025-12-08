fabf258af621e7ac92be61e17fe2238c0321b4df
Aditya Kumar <adityaforwork101@gmail.com>
2025-12-08 18:38:17 +0530

    added 16th project

--- 16-RecipeFinderApp/src/components/Navbar.jsx
+++ 16-RecipeFinderApp/src/components/Navbar.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-        <div className='flex justify-between items-center px-20 bg-cyan-950 text-white'>
-            {/* <h1>RecipeFinder</h1> */}
-            <div className='w-20 h-20'>
-              <img src="../../public/recipefinderlogo.png" className='w-full h-full object-cover' alt="logo" />
+        <div className='flex justify-between items-center md:px-20 px-5 bg-cyan-950 text-white'>
+            <div className='md:w-20 md:h-20 w-15 h-15'>
+              {/* <img src="../../public/recipefinderlogo.png" className='w-full h-full object-cover' alt="logo" /> */}
+              <img src="/recipefinderlogo.png" className='w-full h-full object-cover' alt="logo" />
             </div>
-            <div className='flex items-center gap-16 text-xl text-white'>
-              <Link to="/" className='hover:text-yellow-500 hover:font-semibold'>Home</Link>
-              <Link to="/categories" className='hover:text-yellow-500 hover:font-semibold'>Categories</Link>
-              <Link to="/fav" className='hover:text-yellow-500 hover:font-semibold'>Favourites</Link>
+            <div className='flex items-center md:gap-16 gap-5 md:text-xl text-sm'>
+              <NavLink to="/" className='hover:text-yellow-500 hover:font-semibold' 
+              style={({ isActive }) => ({
+                    color: isActive ? "orange" : "white",
+                    textDecoration: "none",
+                  })}>Home</NavLink>
+              <NavLink to="/categories" className='hover:text-yellow-500 hover:font-semibold' 
+              style={({ isActive }) => ({
+                    color: isActive ? "orange" : "white",
+                    textDecoration: "none",
+                  })}>Categories</NavLink>
+              <NavLink to="/fav" className='hover:text-yellow-500 hover:font-semibold' 
+              style={({ isActive }) => ({
+                    color: isActive ? "orange" : "white",
+                    textDecoration: "none",
+                  })}>Favourites</NavLink>
             </div>
         </div>
     </div>
--- 16-RecipeFinderApp/src/context/FavContext.jsx
+++ 16-RecipeFinderApp/src/context/FavContext.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { createContext, useState, useEffect } from "react";
 
-const FavContext = () => {
+export const FavContext = createContext();
+
+export const FavProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const favData = localStorage.getItem("favData");
+
+    if (favData) {
+      try {
+        setFavorites(JSON.parse(favData));
+      } catch (e) {
+        console.error("Invalid JSON in localStorage:", e);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (favorites.length > 0) {
+      localStorage.setItem("favData", JSON.stringify(favorites));
+    }
+  }, [favorites]);
+
+  const addToFav = (meal) => {
+    setFavorites((prev) => [...prev, meal]);
+  };
+
+  const removeFromFav = (id) => {
+    setFavorites((prev) => prev.filter((item) => item.idMeal !== id));
+  };
+
+  const isFav = (id) => {
+    return favorites.some((item) => item.idMeal === id);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default FavContext
+    <FavContext.Provider value={{ favorites, addToFav, removeFromFav, isFav }}>
+      {children}
+    </FavContext.Provider>
+  );
+};
